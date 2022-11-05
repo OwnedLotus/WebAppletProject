7d151a832a523fb69134e7bc52f99f89fe076851
ownedlotus <JonahRingdahl@outlook.com>
2022-11-04 21:25:14 -0400

    Added comparison to js

--- ProjectPortfolio/wwwroot/main.js
+++ ProjectPortfolio/wwwroot/main.js
@@ -1,5 +1,41 @@
-function returnFilp(x) {
-    return x.flip();
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inputString = "";
+var numberButton = document.querySelectorAll('[data-number]');
+var operationButtons = document.querySelectorAll('[data-operation]');
+var equalsButton = document.querySelector('[data-equals');
+var deleteButton = document.querySelector('[data-delete');
+var allClearButton = document.querySelector('[data-all-clear');
+var previousOperandTextElement = document.querySelector('[data-pervious-operand]');
+var currentOperandTextElement = document.querySelector('[data-current-operand');
+function InsertOne() {
+    inputString += "1";
 }
-var num = 1;
+function InsertTwo() {
+    inputString += "2";
+}
+function InsertThree() {
+    inputString += "3";
+}
+function InsertFour() {
+    inputString += "4";
+}
+function InsertFive() {
+    inputString += "5";
+}
+function InsertSix() {
+    inputString += "6";
+}
+function InsertSeven() {
+    inputString += "7";
+}
+function InsertEight() {
+    inputString += "8";
+}
+function InsertNine() {
+    inputString += "9";
+}
+function InsertZero() {
+    inputString += "0";
+}
 //# sourceMappingURL=main.js.map
--- ProjectPortfolio/wwwroot/main.ts
+++ ProjectPortfolio/wwwroot/main.ts
@@ -1,4 +1,73 @@
+export { };
+
+let inputString: string = "";
+
+const numberButton = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals');
+const deleteButton = document.querySelector('[data-delete');
+const allClearButton = document.querySelector('[data-all-clear');
+const previousOperandTextElement = document.querySelector('[data-pervious-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand');
+
 function InsertOne()
 {
+    inputString += "1";
+}
+function InsertTwo()
+{
+    inputString += "2";
+}
+function InsertThree()
+{
+    inputString += "3";
+}
+function InsertFour()
+{
+    inputString += "4";
+}
+function InsertFive()
+{
+    inputString += "5";
+}
+function InsertSix()
+{
+    inputString += "6";
+}
+function InsertSeven()
+{
+    inputString += "7";
+}
+function InsertEight()
+{
+    inputString += "8";
+}
+function InsertNine()
+{
+    inputString += "9";
+}
+function InsertZero()
+{
+    inputString += "0";
+}
+
+function GeneratePi(iterator: number) {
+    let inCircle: number = 0;
+    let totalCircle: number = 0;
     
+    for (let index = 0; index < iterator; index++) {
+    
+        let firstNum = Math.random(); 
+        let secondNum = Math.random();
+    
+        let dist = Math.sqrt(Math.pow(firstNum, 2) + Math.pow(secondNum, 2));
+    
+        if (dist >= 1) {
+            inCircle++;
+        }
+        totalCircle++;
+        
+    }
+
+    return 4 * (inCircle / totalCircle);
 }
